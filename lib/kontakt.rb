d7863c978a4c2e4fb986bfa5cd079d5c20197516
Greg Winn <greg@winn.ws>
2015-09-12 14:53:28 -0500

    Organized module structure, added Device.by_id and Device.update.

--- lib/kontakt.rb
+++ lib/kontakt.rb
@@ -7,32 +7,42 @@
 require 'rest-client'
 require 'yaml'
 
-class Kontakt
+module Kontakt
   GEM_ROOT = File.expand_path("../..", __FILE__)
   AUTH_INFO = YAML.load_file("#{GEM_ROOT}/config/kontakt.yml")
   API_URL = "https://api.kontakt.io"
-  RESOURCE_DATA = {:accept => "application/vnd.com.kontakt+json;version=5", :"Api-Key" => AUTH_INFO["key"]}
+  RESOURCE_DATA = {:accept => "application/vnd.com.kontakt+json;version=5", :"Api-Key" => AUTH_INFO["key"], :content_type => "application/x-www-form-urlencoded"}
 
-  # => Kontakt Authentication
-  def initialize
-    @resource = RestClient::Resource.new API_URL
+  class Auth
+    # => Kontakt Authentication
+    def self.make_request(method, path, options = {}, payload = {})
+      RestClient::Request.execute(method: method.to_sym, url: API_URL + path, payload: payload, headers: options.merge(RESOURCE_DATA))
+    end
   end
 
-  # ==========================
-  # => Venues
-  def venue_list
-    return @resource['/venue'].get RESOURCE_DATA
-  end
-  
-  # ==========================
-  # => Devices / Beacons
-  def device_list(managerId = [])
-    # => managerId can be manager UUID's in an array
-    return @resource['/device'].get {:params => {:managerId => managerId.join(',')}}, RESOURCE_DATA
+  class Venue < Auth
+    # ==========================
+    # => Venues
+    def self.list
+      return JSON.parse(make_request('get', '/venue').body)
+    end
   end
 
-  def device_by_id(id)
-    return @resource['/device/' + id].get RESOURCE_DATA
+  class Device < Auth
+    # ==========================
+    # => Devices / Beacons
+    def self.list(managerId = [])
+      # => managerId can be manager UUID's in an array
+      return JSON.parse(make_request('get', '/device', {params: {:managerId => managerId.join(',')}}).body)
+    end
+
+    def self.by_id(id)
+      return JSON.parse(make_request('get', '/device/' + id).body)
+    end
+
+    def self.update(id, type, options = {})
+      return make_request('post', '/device/update', {}, {:uniqueId => id, :deviceType => type}.merge(options))
+    end
   end
 
 end
